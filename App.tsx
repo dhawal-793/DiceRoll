ed636cf4fc799ca116fc4b0c2cf846b6f56e1b8a
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-14 17:43:15 +0530

    Removed unnecessary components.

--- App.tsx
+++ App.tsx
@@ -1,44 +1,21 @@
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  Header,
-  LearnMoreLinks
-} from 'react-native/Libraries/NewAppScreen';
-
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+        >
+        <View>
           <Text>
             Dice Roll
           </Text>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
